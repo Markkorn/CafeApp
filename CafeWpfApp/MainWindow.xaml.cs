93a11ce01fc0a99d60fa5127aa1c87be498702c6
Корсак Марк Сергеевич <gr611_komse@ttit.local>
2022-04-29 11:53:19 +0700

    first commit

---
+++ CafeWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CafeWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MessageBox.Show(Helper.Db.Roles.First(q => q.Id == 1).Name
+                );
+        }
+
+        private void LogInBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (LoginTBox.Text == "" || PasswordPBox.Password == "")
+            {
+                MessageBox.Show("Введите логин и пароль");
+            }
+            else
+            {
+                User user = Helper.Db.Users.FirstOrDefault(q => q.Login == LoginTBox.Text && q.Password == PasswordPBox.Password);
+
+                if (user != null)
+                {
+                    if (user.RoleId == 1)
+                    {
+                        new Waiter().Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        new Cook().Show();
+                        this.Close();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Неправильный логин и пароль");
+                }
+                }
+            }
+        }
+    } 
+
